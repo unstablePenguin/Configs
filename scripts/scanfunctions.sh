f54b4e3ce82ae6f2332018a391e697da0890f8c1
unstablePenguin <niftycello@pm.me>
2023-05-25 22:42:27 -0400

    Made scanfuctions work and more simple. Created misc tricks for viewing script typescript.

--- scripts/scanfunctions.sh
+++ scripts/scanfunctions.sh
@@ -1,61 +1,48 @@
 #!/bin/bash
 #
-
-
-#seq 100 200 | xargs -I{} -P0 nmap -Pn -sT -p 22,23,80 192.168.100.{} --open -oG - |grep -v '#'| grep -P '(report|open)' | sed 's|(\(1\))|\n|g' | sed -E 's|^(.+\s)I.+$|\1|'
+#
+PROMPT="Usage: $0 {NetworkAddr/CIDR}\nEx. $0 172.16.31.0/24 "
+if [[ $# < 1 ]];
+	printf "${PROMPT}\n"
 PORTS="22,23,80"
-_network=testnet
+_network=$1
+logfile=${_network}/${_network}.log
 
-#seq 1 254 | xargs -I{} -P0 nmap -Pn -sT -n -p 23,23,80 192.168.8.{}  --open | rg '(^Nmap|^\d)' | awk '/Nmap/{print $NF} /^[0-9]/{print $0}' 
+if [[ -d $_network ]];then
+	cd $_network
+else
+    printf "[-] Creating Directory for ${_network}\n" 
+    mkdir $_network
+    cd $_network
+fi
+[[ -e $logfile ]] && mv $logfile ${logfile}.old || touch $logfile
+[[ -d $network ]] && printf "[+] Directory Created\n" | tee -a $logfile
 
-_hosts=$(seq 1 254 | xargs -I{} -P0 nmap -Pn -sT -n -p $PORTS -oN - 192.168.8.{}  --open | rg '(^Nmap|^\d)' | awk '/Nmap/{print $NF}')
+printf "[-] Scanning for hosts on ${_network}\n" | tee -a $logfile
+_hosts=$(nmap -sn -n -oG - $_network | awk '/Up/{print $2}')
+printf "[+] Host Discovery Complete\n" | tee -a $logfile
+echo $_hosts | tee -a $logfile
 
-mkdir $_network
-cd $_network
-
+printf "[-] Starting Portscan\n" | tee -a $logfile
 for i in $_hosts;do
+	printf "[-] Creating directory for ${i}\n"
 	mkdir $i
-	nmap -Pn -sT -n -p $PORTS --open -oN ${i}/scan.nmap $i | awk '/^Nmap/{print $NF} /^[0-9]/{print $0}'
-	traceroute $i >> ${i}/traceroute &
-done
+	nmap -Pn -sT -n -p $PORTS --open --traceroute -oN ${i}/scan.nmap $i
+	printf "[+] Portscan Complete for ${i}\n" | tee -a $logfile
+	cat ${i}/scan.nmap | tee -a $logfile
+	grep "80/tcp open" ${i}/scan.nmap
+        if [[ $? == 0 ]];then
+	    printf "[-] Checking ${i} Version\n" | tee -a $logfile
+######	    curl http://${i}/  2>/dev/null | grep "##pattern##" | tee -a ${i}/version.txt
+	    printf "[+] ${i} Version: $(cat ${i/version.txt})\n" | tee -a $logfile
 
-for i in $_hosts;do
-	printf "Checking ${i}: \n"
-	IFS=","
-	for p in $PORTS;do
-		case $p in 
-			22)
-	                    grep "22/tcp open" ${i}/scan.nmap
-	                    if [[ $? == 0 ]];then
-				    printf "Checking Version...\n"
-				    ver_ck=true
-				    if [[ ver_ck ]];then
-				        printf "Sucks to suck\n"
-				    fi
-			    fi
-				;;
-			80)
-	                    grep "80/tcp open" ${i}/scan.nmap
-	                    if [[ $? == 0 ]];then
-				    printf "Checking Version...\n"
-				    curl http://${i}/ -o ${i}/curl.out 2>/dev/null
-				    ver_ck=true
-				    if [[ ver_ck ]];then
-				        printf "Sucks to suck\n"
-				    fi
-			    fi
-				;;
-			23)
-	                    grep "23/tcp open" ${i}/scan.nmap
-	                    if [[ $? == 0 ]];then
-				    printf "Checking Version...\n"
-				    ver_ck=false
-				    if [[ ver_ck ]];then
-				        printf "Sucks to suck\n"
-				    fi
-			    fi
-				;;
-		esac
-	done
-
+	fi
+#####	grep "23/tcp open" ${i}/scan.nmap
+	if [[ $? == 0 ]];then
+	    printf "[-] Checking ${i} Version\n" | tee -a $logfile
+#####	    true | tee -a ${i}/version.txt
+	    printf "[+] ${i} Version: $(cat ${i/version.txt})\n" | tee -a $logfile
+	fi
+	    
+        	
 done
